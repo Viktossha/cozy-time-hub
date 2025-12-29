4befe4d27849d829671ae4a157e0f2d6efe39877
Vika <vikapetrova8@gmail.com>
2025-12-30 00:42:58 +0300

    first commit

---
+++ src/shared/components/Button.tsx
@@ -0,0 +1,11 @@
+import type {ComponentPropsWithoutRef} from "react";
+
+type Props = {} & ComponentPropsWithoutRef<'button'>
+
+export const Button = ({}: Props) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
