9bc1d13cbe83511b88e7c210e7983fd390e7437b
Vika <vikapetrova8@gmail.com>
2025-12-30 00:59:46 +0300

    refactor: move Stopwatch to separated component

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,10 @@
-import {useRef, useState} from "react";
+import {Stopwatch} from "./features/stopwatch/Stopwatch.tsx";
 
 function App() {
 
-    let [time, setTime] = useState(0)
-
-    let [isIntervalStarted, setIsIntervalStarted] = useState(false)
-    let intervalId = useRef<undefined | number>(undefined)
-
-    let seconds = time % 60
-    let minutes = Math.floor(time / 60) % 60
-    let hours = Math.floor(time / 3600) % 24
-    let days =  Math.floor(time / 86400)
-
-
-    const onStartHandler = () => {
-        setIsIntervalStarted(true)
-        intervalId.current = setInterval(() => {
-            setTime(prevState => prevState + 1)
-        }, 1000)
-    }
-
-    const onPauseHandler = () => {
-        if (intervalId) {
-            clearInterval(intervalId.current)
-            setIsIntervalStarted(false)
-            intervalId.current = undefined
-        }
-    }
-
-    const onResetHandler = () => {
-        clearInterval(intervalId.current)
-        setIsIntervalStarted(false)
-        setTime(0)
-    }
-
-
-    return (
-        <div className={'border bg-blue-300 rounded-md mt-15 flex flex-col items-center p-4'}>
-            <div className={'flex gap-10'}>
-                <div>
-                    <div className={'bg-blue-500 border rounded-md text-center'}>{days}</div>
-                    <span>days</span>
-                </div>
-                <div>
-                    <div className={'bg-blue-500 border rounded-md text-center'}>{hours}</div>
-                    <span>hours</span>
-                </div>
-                <div>
-                    <div className={'bg-blue-500 border rounded-md text-center'}>{minutes}</div>
-                    <span>minutes</span>
-                </div>
-                <div>
-                    <div className={'bg-blue-500 border rounded-md text-center'}>{seconds}</div>
-                    <span>seconds</span>
-                </div>
-            </div>
-
-            <div className={'flex gap-10'}>
-                {isIntervalStarted ?
-                    <button onClick={onPauseHandler}>pause</button>
-                    : <button onClick={onStartHandler}>start</button>}
-
-                {time !== 0 && <button onClick={onResetHandler}>reset</button>}
-            </div>
-        </div>
-    )
+    return <>
+        <Stopwatch/>
+    </>
 }
 
 export default App
