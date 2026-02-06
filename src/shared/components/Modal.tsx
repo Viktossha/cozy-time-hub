1c62325d782fc06c343711a189219447bdafcfe8
Vika <vikapetrova8@gmail.com>
2026-02-06 14:47:28 +0300

    styles: update Modal styles, add close button styling

--- src/shared/components/Modal.tsx
+++ src/shared/components/Modal.tsx
@@ -1,19 +1,21 @@
 import * as Dialog from '@radix-ui/react-dialog';
 import type {DialogProps} from '@radix-ui/react-dialog';
 import {Button} from "./Button.tsx";
+import {cn} from "../lib/cn.ts";
 
 type Props = {
     title?: string
     description?: string
     closeText?: string
+    className?: string
 } & DialogProps
-export const Modal = ({open, onOpenChange, title, description, closeText, children, ...dialogProps}: Props) => {
+export const Modal = ({open, onOpenChange, title, description, closeText, children, className, ...dialogProps}: Props) => {
     return (
         <Dialog.Root open={open} onOpenChange={onOpenChange} {...dialogProps}>
             <Dialog.Portal>
                 <Dialog.Overlay className="fixed inset-0 data-[state=open]:animate-overlayShow"/>
                 <Dialog.Content
-                    className="bg-slate-900/90 backdrop-blur-2xl fixed left-1/2 top-1/2 max-h-[85vh] w-[90vw] max-w-[500px] -translate-x-1/2 -translate-y-1/2 rounded-xl p-[25px] border border-white/10 shadow-(--shadow-6) focus:outline-none data-[state=open]:animate-contentShow">
+                    className={cn("bg-slate-900/90 backdrop-blur-2xl fixed left-1/2 top-1/2 max-h-[85vh] w-full max-w-[500px] -translate-x-1/2 -translate-y-1/2 rounded-xl p-[25px] border border-white/10 shadow-(--shadow-6) focus:outline-none data-[state=open]:animate-contentShow", className)}>
                     {title && <Dialog.Title className="m-0 text-lg font-medium">
                         {title}
                     </Dialog.Title>}
@@ -24,12 +26,12 @@ export const Modal = ({open, onOpenChange, title, description, closeText, childr
                     <Dialog.Close asChild
                                   className="absolute right-2.5 top-2.5 inline-flex size-[25px] appearance-none items-center justify-center rounded-full text-white/40 hover:text-white/80 cursor-pointer focus:shadow-[0_0_0_2px] focus:shadow-violet7 focus:outline-none"
                                   aria-label="Close">
-                        <button>✕</button>
+                        <Button className={'bg-none bg-transparent border-none'} hoverScale={false}>✕</Button>
                     </Dialog.Close>
                     {closeText &&
                         <Dialog.Close asChild
-                                      className="hover:bg-violet4 cursor-pointer focus:shadow-[0_0_0_2px] focus:shadow-violet7 focus:outline-none">
-                            <Button>{closeText}</Button>
+                                      className="mx-auto block mt-10 hover:bg-violet4 cursor-pointer focus:shadow-[0_0_0_2px] focus:shadow-violet7 focus:outline-none">
+                            <Button className={'px-10'}>{closeText}</Button>
                         </Dialog.Close>}
                 </Dialog.Content>
             </Dialog.Portal>
