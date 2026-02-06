ed693974b59cf0f04a945bd40aaefba79e2691fe
Vika <vikapetrova8@gmail.com>
2026-02-06 14:54:34 +0300

    feat: add hoverScale prop to Button

--- src/shared/components/Button.tsx
+++ src/shared/components/Button.tsx
@@ -1,11 +1,11 @@
 import type {ComponentPropsWithoutRef} from "react";
 import {cn} from "../lib/cn.ts";
 
-type Props = ComponentPropsWithoutRef<'button'>
+type Props = {hoverScale?: boolean} & ComponentPropsWithoutRef<'button'>
 
-export const Button = ({className, children, ...rest}: Props) => {
+export const Button = ({hoverScale = true, className, children, ...rest}: Props) => {
     return (
-        <button {...rest} className={cn('rounded-xl bg-cyan-500/20 backdrop-blur-xl border-cyan-400/30 hover:bg-cyan-500/30 text-cyan-200 hover:scale-105 active:scale-95 border transition-all duration-300 px-6 py-3', className)}>
+        <button {...rest} className={cn('rounded-xl bg-cyan-500/20 backdrop-blur-xl border-cyan-400/30 hover:bg-cyan-500/30 text-cyan-200 active:scale-95 border transition-all duration-300 px-6 py-3', {'hover:scale-105': hoverScale}, className)}>
             {children}
         </button>
     );
