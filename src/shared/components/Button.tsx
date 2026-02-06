c7360799bcf09d8ac90e9e34117b693793997b77
Vika <vikapetrova8@gmail.com>
2026-02-06 12:20:22 +0300

    feat: add reusable button component

--- src/shared/components/Button.tsx
+++ src/shared/components/Button.tsx
@@ -1,11 +1,15 @@
-import type {ComponentPropsWithoutRef} from "react";
+import type {ComponentPropsWithoutRef, ReactNode} from "react";
+import {cn} from "../lib/cn.ts";
 
-type Props = {} & ComponentPropsWithoutRef<'button'>
+type Props = {
+    children: ReactNode
+    className?: string
+} & ComponentPropsWithoutRef<'button'>
 
-export const Button = ({}: Props) => {
+export const Button = ({className, children, ...rest}: Props) => {
     return (
-        <div>
-
-        </div>
+        <button {...rest} className={cn('rounded-xl bg-cyan-500/20 backdrop-blur-xl border-cyan-400/30 hover:bg-cyan-500/30 text-cyan-200 hover:scale-105 active:scale-95 border transition-all duration-300 px-6 py-3', className)}>
+            {children}
+        </button>
     );
 };
