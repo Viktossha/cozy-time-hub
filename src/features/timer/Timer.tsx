a2df34cc6f493c7ec41f73dfd9651c33f05021cf
Viktossha <vikapetrova8@gmail.com>
2026-03-01 21:57:08 +0300

    test: test commit

--- src/features/timer/Timer.tsx
+++ src/features/timer/Timer.tsx
@@ -1,12 +1,53 @@
-import {useState} from "react";
-import {cn} from "@/shared/lib/cn.ts";
+import {useRef, useState} from "react";
 import {Button} from "@/shared/components/Button.tsx";
 import {Modal} from "@/shared/components/Modal.tsx";
 import {Input} from "@/shared/components/Input.tsx";
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/shared/components/Tabs.tsx";
 
 export const Timer = () => {
     const [showSettings, setShowSettings] = useState(false)
-    const [activeTab, setActiveTab] = useState<'duration' | 'targetDate'>('duration')
+    const [time, setTime] = useState(0)
+    const [isRunning, setIsRunning] = useState(false)
+
+
+    const [inputHoursValue, setInputHoursValue] = useState<number>(0)
+    const [inputMinutesValue, setInputMinutesValue] = useState<number>(0)
+    const [inputSecondsValue, setInputSecondsValue] = useState<number>(0)
+
+    const seconds = time % 60
+    const minutes = Math.floor(time / 60) % 60
+    const hours = Math.floor(time / 3600) % 24
+    const days = Math.floor(time / 86400)
+
+    const intervalId = useRef<undefined | number>(undefined)
+
+
+
+    const onStartHandler = () => {
+        if (intervalId.current !== undefined) return
+
+        intervalId.current = setInterval(() => {
+            setTime(prevState => {
+                if (prevState <= 1) {
+                    setIsRunning(false)
+                    clearInterval(intervalId.current)
+                    return 0
+                }
+                return prevState - 1
+            })
+        }, 1000)
+
+        setIsRunning(true)
+    }
+
+    const onSetTimerSettings = () => {
+        setTime(inputHoursValue * 3600 + inputMinutesValue * 60 + inputSecondsValue)
+    }
+
+    const onResetHandler = () => {
+        clearInterval(intervalId.current)
+        setTime(0)
+    }
 
     return (
         <div
@@ -14,54 +55,57 @@ export const Timer = () => {
             <h3>Таймер</h3>
             <div className={'flex gap-10'}>
                 <div>
-                    <div className={'bg-white/5 backdrop-blur-md border rounded-md text-center'}>{0}</div>
+                    <div className={'bg-white/5 backdrop-blur-md border rounded-md text-center'}>{days}</div>
                     <span>days</span>
                 </div>
                 <div>
-                    <div className={'bg-white/5 backdrop-blur-md border rounded-md text-center'}>{0}</div>
+                    <div className={'bg-white/5 backdrop-blur-md border rounded-md text-center'}>{hours}</div>
                     <span>hours</span>
                 </div>
                 <div>
-                    <div className={'bg-white/5 backdrop-blur-md border rounded-md text-center'}>{0}</div>
+                    <div className={'bg-white/5 backdrop-blur-md border rounded-md text-center'}>{minutes}</div>
                     <span>minutes</span>
                 </div>
                 <div>
-                    <div className={'bg-white/5 backdrop-blur-md border rounded-md text-center'}>{0}</div>
+                    <div className={'bg-white/5 backdrop-blur-md border rounded-md text-center'}>{seconds}</div>
                     <span>seconds</span>
                 </div>
             </div>
 
             <div className={'flex gap-10'}>
 
+                {isRunning && time !== 0 && <Button onClick={onResetHandler}>Reset</Button>}
+
+                {time !== 0 && !isRunning && <Button onClick={onStartHandler}>Start</Button>}
+
                 <Button onClick={() => {
                     setShowSettings(true)
-                }}>setTimer</Button>
+                }}>Set Timer</Button>
             </div>
 
             <Modal open={showSettings} onOpenChange={setShowSettings} title={'Set Timer'} closeText={'Set'}
-                   className={'max-w-[448px]'}>
-                <div className={'flex gap-1.5 p-1 bg-white/5 rounded-xl mt-4 mb-6'}>
-                    <Button hoverScale={false} className={cn('bg-transparent  bg-linear-to-r from-cyan-500/20 to-violet-500/20 border border-cyan-400/30 w-full transition-all duration-200 hover:bg-white/5 ', {'bg-none border-none text-white/40': activeTab !== 'duration'})} onClick={() => setActiveTab('duration')}>Duration</Button>
-                    <Button hoverScale={false} className={cn('bg-transparent  bg-linear-to-r from-cyan-500/20 to-violet-500/20 border border-cyan-400/30 w-full transition-all duration-200 hover:bg-white/5', {'bg-none border-none text-white/40': activeTab !== 'targetDate'})}  onClick={() => setActiveTab('targetDate')}>Target Date</Button>
-                </div>
+                   className={'max-w-[448px]'} onCloseTextButton={onSetTimerSettings}>
 
-                {activeTab === 'duration' && <>
-                    <p className={'uppercase text-sm text-white/40 font-medium mb-2'}>Set
-                    duration</p>
-                    <div className={'flex w-full gap-4'}>
-                        <Input className="flex-1" label={'Hours'} type={'number'} max={23} min={0}/>
-                        <Input className="flex-1" label={'Minutes'} type={'number'} max={59} min={0}/>
-                        <Input className="flex-1" label={'Seconds'} type={'number'} max={59} min={0}/>
-                    </div>
-                </>}
-
-                {activeTab === 'targetDate' && <>
-                    <p className={'uppercase text-sm text-white/40 font-medium mb-2'}>Target Date & Time</p>
-                    <Input type="datetime-local"/>
-                </>}
+                <Tabs defaultValue="duration" className="w-[400px]">
+                    <TabsList>
+                        <TabsTrigger value="duration">Duration</TabsTrigger>
+                        <TabsTrigger value="targetDate">Target Date</TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="duration"><p className={'uppercase text-sm text-white/40 font-medium mb-2'}>Set
+                        duration</p>
+                        <div className={'flex w-full gap-4'}>
+                            <Input className="flex-1" label={'Hours'} type={'number'} max={99} min={0} value={inputHoursValue} onValueChange={(value) => setInputHoursValue(+value)}/>
+                            <Input className="flex-1" label={'Minutes'} type={'number'} max={59} min={0}  value={inputMinutesValue}  onValueChange={(value) => setInputMinutesValue(+value)}/>
+                            <Input className="flex-1" label={'Seconds'} type={'number'} max={59} min={0}  value={inputSecondsValue}  onValueChange={(value) => setInputSecondsValue(+value)}/>
+                        </div></TabsContent>
+                    <TabsContent value="targetDate"> <p className={'uppercase text-sm text-white/40 font-medium mb-2'}>Target Date & Time</p>
+                        <Input type="datetime-local"/></TabsContent>
+                </Tabs>
 
 
             </Modal>
+
+
         </div>
     );
 };
