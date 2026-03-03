60934ed7768fc748b68af67081dd70df75f72c63
Vika <vikapetrova8@gmail.com>
2026-03-04 00:00:53 +0300

    feat: add icons for buttons in Stopwatch

--- src/features/stopwatch/Stopwatch.tsx
+++ src/features/stopwatch/Stopwatch.tsx
@@ -1,4 +1,6 @@
 import {useRef, useState} from "react";
+import {Button} from "@/shared/components/Button.tsx";
+import {Pause, Play, TimerReset} from "lucide-react";
 
 export const Stopwatch = () => {
     let [time, setTime] = useState(0)
@@ -38,10 +40,9 @@ export const Stopwatch = () => {
 
     return (
         <div
-            className={'border  bg-linear-to-r from-cyan-500/20 via-violet-500/10 to-cyan-500/20 rounded-md mt-15 flex flex-col items-center p-4'}>
+            className={'mt-15 flex flex-col items-center p-4 gap-10'}>
             {/*<div*/}
             {/*    className={' bg-white/3 backdrop-blur-2xl rounded-2xl border border-white/10'}>*/}
-                <h3>Секундомер</h3>
                 <div className={'flex gap-10'}>
                     <div>
                         <div className={'bg-white/5 backdrop-blur-md border rounded-md text-center'}>{days}</div>
@@ -63,10 +64,10 @@ export const Stopwatch = () => {
 
                 <div className={'flex gap-10'}>
                     {isRunning ?
-                        <button onClick={onPauseHandler}>pause</button>
-                        : <button onClick={onStartHandler}>start</button>}
+                        <Button onClick={onPauseHandler} icon={<Pause size={16}/>}>Pause</Button>
+                        : <Button onClick={onStartHandler} className={'flex items-center justify-center gap-2'} icon={<Play size={16}/>}>Start</Button>}
 
-                    {time !== 0 && <button onClick={onResetHandler}>reset</button>}
+                    {time !== 0 && <Button onClick={onResetHandler} icon={<TimerReset size={16}/>}>Reset</Button>}
                 </div>
             {/*</div>*/}
 
