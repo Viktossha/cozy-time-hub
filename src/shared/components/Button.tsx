3e65174d971e5278339594c8f4baf5f62dfa0b7b
Vika <vikapetrova8@gmail.com>
2026-03-03 23:56:13 +0300

    feat: add icon prop to Button

--- src/shared/components/Button.tsx
+++ src/shared/components/Button.tsx
@@ -1,11 +1,15 @@
-import type {ComponentPropsWithoutRef} from "react";
+import type {ComponentPropsWithoutRef, ReactNode} from "react";
 import {cn} from "@/shared/lib/cn.ts";
 
-type Props = {hoverScale?: boolean} & ComponentPropsWithoutRef<'button'>
+type Props = {
+    hoverScale?: boolean
+    icon?: ReactNode
+} & ComponentPropsWithoutRef<'button'>
 
-export const Button = ({hoverScale = true, className, children, ...rest}: Props) => {
+export const Button = ({hoverScale = true, icon, className, children, ...rest}: Props) => {
     return (
-        <button {...rest} className={cn('rounded-xl bg-cyan-500/20 backdrop-blur-xl border-cyan-400/30 hover:bg-cyan-500/30 text-cyan-200 active:scale-95 border transition-all duration-300 px-6 py-3', {'hover:scale-105': hoverScale}, className)}>
+        <button {...rest} className={cn('rounded-xl bg-cyan-500/20 backdrop-blur-xl border-cyan-400/30 hover:bg-cyan-500/30 text-cyan-200 active:scale-95 border transition-all duration-300 px-6 py-3', {'hover:scale-105': hoverScale, 'flex items-center justify-center gap-2': icon}, className)}>
+            {icon}
             {children}
         </button>
     );
